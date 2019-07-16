56472cf20c1a8012c5c6a30217c637948713c8a8
Emily Lagattolla <elagattolla@gmail.com>
2019-07-16 15:21:19 -0500

    mapped over arrays to render components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -15,9 +15,7 @@ const Numbers = () => {
       /* STEP 3 - Use .map() to iterate over your array data and return a button
       component matching the name on the provided file. Pass
       it any props needed by the child component*/
-      {numbers.map( () =>
-
-      )};
+      {numbers.map( () => setNumberState(props.numbers) )};
     </div>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -12,9 +12,10 @@ const Specials = () => {
 
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
+      /* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       {specials.map( () => setSpecialState() )};
     </div>
   );
 };
