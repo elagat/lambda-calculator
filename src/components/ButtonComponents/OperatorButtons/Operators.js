1684ec9d602075ed11c4c9e22907e574619ec6c5
Emily Lagattolla <elagattolla@gmail.com>
2019-07-16 15:10:46 -0500

    imported buttons

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 //import any components needed
+import OperatorButton from ./OperatorButton;
 
 //Import your array data to from the provided data file
 import {operators} from "./../../data.js";
