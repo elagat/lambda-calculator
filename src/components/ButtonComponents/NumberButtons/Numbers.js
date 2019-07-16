4b36b65f137a24baa4b6dabcaf280568ac56c8b0
Emily Lagattolla <elagattolla@gmail.com>
2019-07-16 15:09:54 -0500

    added export default to button files

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 //import any components needed
+import NumberButton from ./NumberButton;
 
 //Import your array data to from the provided data file
 import {numbers} from "./../../data.js";
@@ -11,9 +12,12 @@ const Numbers = () => {
 
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      /* STEP 3 - Use .map() to iterate over your array data and return a button
+      component matching the name on the provided file. Pass
+      it any props needed by the child component*/
+      {numbers.map( () =>
+
+      )};
     </div>
   );
 };
