231d319aa5a28a286f516783d6422da2e15555e6
Emily Lagattolla <elagattolla@gmail.com>
2019-07-17 07:28:54 -0500

    WIP: creating JSX in Display

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
+
+import Numbers from "../ButtonComponents/NumberButtons/Numbers.js";
+import Operators from "../ButtonComponents/OperatorButtons/Operators.js";
+import Specials from "../ButtonComponents/SpecialButtons/Specials.js";
 
 const Display = () => {
+  const [displayState, setDisplayState] = useState()
   return <div>{/* Display any props data here */}</div>;
 };
+
+export default Display;
