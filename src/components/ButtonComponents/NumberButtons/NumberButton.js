145c7ccf2c6e62517ac56f1b7ac67571f2398bfd
Emily Lagattolla <elagattolla@gmail.com>
2019-07-17 09:49:43 -0500

    wrote logic to have larger 0 button

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,15 @@
 import React, {useState} from "react";
 
+import {numbers} from "../../../data.js";
+
 const NumberButton = (props) => {
-  return (
-    <button>{props.number}</button>
-  );
+  const [numberButtonState, setNumberButtonState] = useState(numbers);
+
+  if (props.number === "0") {
+    return <button className="zero">{props.number}</button>
+  } else {
+    return <button>{props.number}</button>
+  }
 };
 
 export default NumberButton;
